4b01476f269e1e18d03fc3e4a2af959d0bc6f66f
Rohit Kumar Mahto <rohitmahto062@gmail.com>
2025-01-21 16:41:37 +0530

    Project and skill added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { Banner } from './components/Banner';
 import { NavBar } from './components/NavBaar';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-  <NavBar />
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
     </div>
   );
 }
